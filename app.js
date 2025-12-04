cd9fb4e6599541098fa561d579fc197e4720d607
Anay_Gupta <anayg.hestabit@gmail.com>
2025-12-04 12:32:54 +0530

    fix: commit-7 fix syntax error

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ console.log("Hello from commit 1");
 console.log("This is commit 2");
 function add(a,b){ return a+b; }
 function sub(a,b){ return a-b; }
-console.log("BUG HERE" 
+console.log("BUG HERE");
 console.log("Commit 6, still broken");
